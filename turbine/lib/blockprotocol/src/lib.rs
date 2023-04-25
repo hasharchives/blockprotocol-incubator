06aeb48252c9f4145539bdbd371bbd79c9e65dce
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-04-25 18:01:28 +0200

    feat: implement `Type`

--- turbine/lib/blockprotocol/src/lib.rs
+++ turbine/lib/blockprotocol/src/lib.rs
@@ -1,1 +1,76 @@
+use std::borrow::Borrow;
+
+use type_system::url::{BaseUrl, VersionedUrl};
+
 pub mod types;
+
+#[derive(Debug, Copy, Clone)]
+pub struct BaseUrlRef<'a>(&'a str);
+
+impl<'a> BaseUrlRef<'a> {
+    #[doc(hidden)] // use the macro instead
+    #[must_use]
+    pub const fn new_unchecked(url: &'a str) -> Self {
+        Self(url)
+    }
+
+    // cannot implement ToOwned because this is fallible
+    // TODO: compile time fail! ~> const fn validator?
+    #[must_use]
+    pub fn into_owned(self) -> BaseUrl {
+        BaseUrl::new(self.0.to_owned()).expect("invalid Base URL")
+    }
+}
+
+pub struct VersionedUrlRef<'a> {
+    base: BaseUrlRef<'a>,
+    version: u32,
+}
+
+impl<'a> VersionedUrlRef<'a> {
+    #[doc(hidden)] // use the macro instead
+    #[must_use]
+    pub const fn new_unchecked(base: BaseUrlRef<'a>, version: u32) -> Self {
+        Self { base, version }
+    }
+
+    #[must_use]
+    pub const fn base(&self) -> BaseUrlRef<'a> {
+        self.base
+    }
+
+    #[must_use]
+    pub const fn version(&self) -> u32 {
+        self.version
+    }
+}
+
+#[macro_export]
+macro_rules! url {
+    ($base:literal / v / $version:literal) => {
+        $crate::VersionedUrlRef::new_unchecked($crate::BaseUrlRef::new_unchecked($base), $version)
+    };
+}
+
+pub trait TypeRef {
+    type Owned;
+
+    // called into_owned instead of to_owned to prevent confusion
+    fn into_owned(self) -> Self::Owned;
+}
+
+pub trait Type {
+    type Ref<'a>: TypeRef<Owned = Self>
+    where
+        Self: 'a;
+
+    const ID: VersionedUrlRef<'static>;
+
+    fn as_ref(&self) -> Self::Ref<'_>;
+}
+
+pub trait DataType: Type {}
+
+pub trait PropertyType: Type {}
+
+pub trait EntityType: Type {}
--- turbine/lib/blockprotocol/src/types/data.rs
+++ turbine/lib/blockprotocol/src/types/data.rs
@@ -1,5 +1,7 @@
 use std::ops::Deref;
 
+use crate::{url, Type, TypeRef, VersionedUrlRef};
+
 pub struct Text(String);
 
 impl Deref for Text {
@@ -10,7 +12,26 @@ impl Deref for Text {
     }
 }
 
+impl Type for Text {
+    type Ref<'a> = TextRef<'a> where Self: 'a;
+
+    const ID: VersionedUrlRef<'static> =
+        url!("https://blockprotocol.org/@blockprotocol/types/data-type/text" / v / 1);
+
+    fn as_ref(&self) -> Self::Ref<'_> {
+        TextRef(&self.0)
+    }
+}
+
 pub struct TextRef<'a>(&'a str);
+
+impl TypeRef for TextRef<'_> {
+    type Owned = Text;
+
+    fn into_owned(self) -> Self::Owned {
+        Text(self.0.to_owned())
+    }
+}
 
 impl Deref for TextRef<'_> {
     type Target = str;
@@ -30,6 +51,17 @@ impl Deref for Number {
     }
 }
 
+impl Type for Number {
+    type Ref<'a> = NumberRef<'a> where Self: 'a;
+
+    const ID: VersionedUrlRef<'static> =
+        url!("https://blockprotocol.org/@blockprotocol/types/data-type/number" / v / 1);
+
+    fn as_ref(&self) -> Self::Ref<'_> {
+        NumberRef(&self.0)
+    }
+}
+
 pub struct NumberRef<'a>(&'a serde_json::Number);
 
 impl Deref for NumberRef<'_> {
@@ -40,6 +72,15 @@ impl Deref for NumberRef<'_> {
     }
 }
 
+impl TypeRef for NumberRef<'_> {
+    type Owned = Number;
+
+    fn into_owned(self) -> Self::Owned {
+        Number(self.0.clone())
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
 pub struct Boolean(bool);
 
 impl Deref for Boolean {
@@ -50,11 +91,81 @@ impl Deref for Boolean {
     }
 }
 
+impl Type for Boolean {
+    type Ref<'a> = Self where Self: 'a;
+
+    const ID: VersionedUrlRef<'static> =
+        url!("https://blockprotocol.org/@blockprotocol/types/data-type/boolean" / v / 1);
+
+    fn as_ref(&self) -> Self::Ref<'_> {
+        *self
+    }
+}
+
+impl TypeRef for Boolean {
+    type Owned = Self;
+
+    fn into_owned(self) -> Self::Owned {
+        self
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
 pub struct Null;
 
+impl Type for Null {
+    type Ref<'a> = Self where Self: 'a;
+
+    const ID: VersionedUrlRef<'static> =
+        url!("https://blockprotocol.org/@blockprotocol/types/data-type/null" / v / 1);
+
+    fn as_ref(&self) -> Self::Ref<'_> {
+        *self
+    }
+}
+
+impl TypeRef for Null {
+    type Owned = Self;
+
+    fn into_owned(self) -> Self::Owned {
+        self
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
 pub struct EmptyList;
 
+impl Type for EmptyList {
+    type Ref<'a> = Self where Self: 'a;
+
+    const ID: VersionedUrlRef<'static> =
+        url!("https://blockprotocol.org/@blockprotocol/types/data-type/emptyList" / v / 1);
+
+    fn as_ref(&self) -> Self::Ref<'_> {
+        *self
+    }
+}
+
+impl TypeRef for EmptyList {
+    type Owned = Self;
+
+    fn into_owned(self) -> Self::Owned {
+        self
+    }
+}
+
 pub struct Object(serde_json::Map<String, serde_json::Value>);
+
+impl Type for Object {
+    type Ref<'a> = ObjectRef<'a> where Self: 'a;
+
+    const ID: VersionedUrlRef<'static> =
+        url!("https://blockprotocol.org/@blockprotocol/types/data-type/object" / v / 1);
+
+    fn as_ref(&self) -> Self::Ref<'_> {
+        ObjectRef(&self.0)
+    }
+}
 
 impl Deref for Object {
     type Target = serde_json::Map<String, serde_json::Value>;
@@ -71,5 +182,13 @@ impl Deref for ObjectRef<'_> {
 
     fn deref(&self) -> &Self::Target {
         self.0
+    }
+}
+
+impl TypeRef for ObjectRef<'_> {
+    type Owned = Object;
+
+    fn into_owned(self) -> Self::Owned {
+        Object(self.0.clone())
     }
 }
