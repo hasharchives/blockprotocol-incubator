fb47147bd009d37c0264bf3d13ca2c77720d54ae
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-05-31 20:00:08 +0200

    feat: render toml

--- libs/turbine/lib/skeletor/src/cargo.rs
+++ libs/turbine/lib/skeletor/src/cargo.rs
@@ -1,26 +1,24 @@
 use std::fmt::{Display, Formatter};
 
-use error_stack::{IntoReport, Report, ResultExt};
+use askama::Template;
+use error_stack::{IntoReport, Report, Result, ResultExt};
 use url::Url;
 
-use crate::Error;
+use crate::{Config, Dependency, Error};
 
-#[derive(Debug, Clone, serde::Serialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 struct Index {
     name: String,
     version: String,
 }
 
-async fn fetch_version(name: &str) -> Result<String, Error> {
+fn fetch_version(name: &str) -> Result<String, Error> {
     let url = Url::parse("https://index.crates.io/").expect("failed to parse url");
 
+    #[allow(clippy::string_slice)]
     let url = match name.len() {
-        1 => url
-            .join(&format!("1/{}", name))
-            .expect("failed to join url"),
-        2 => url
-            .join(&format!("2/{}", name))
-            .expect("failed to join url"),
+        1 => url.join(&format!("1/{name}")).expect("failed to join url"),
+        2 => url.join(&format!("2/{name}")).expect("failed to join url"),
         3 => url
             .join(&format!("3/{}/{}", &name[..1], name))
             .expect("failed to join url"),
@@ -29,16 +27,11 @@ async fn fetch_version(name: &str) -> Result<String, Error> {
             .expect("failed to join url"),
     };
 
-    let response = reqwest::get(url)
-        .await
+    let response = reqwest::blocking::get(url)
         .into_report()
         .change_context(Error::Http)?;
 
-    let text = response
-        .text()
-        .await
-        .into_report()
-        .change_context(Error::Http)?;
+    let text = response.text().into_report().change_context(Error::Http)?;
 
     let latest = text
         .lines()
@@ -52,6 +45,7 @@ async fn fetch_version(name: &str) -> Result<String, Error> {
     Ok(index.version)
 }
 
+#[derive(Debug, Clone)]
 pub enum TurbineVersion {
     Path(String),
     Git {
@@ -62,15 +56,63 @@ pub enum TurbineVersion {
     },
 }
 
-impl Display for TurbineVersion {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            TurbineVersion::Path(path) => f.write_fmt(format_args!(r##"{{ path = "{path}" }}"##)),
-            TurbineVersion::Git { .. } => {}
+// Reason: it is only fallible in a case which we don't support just yet.
+#[allow(clippy::fallible_impl_from)]
+impl From<Dependency> for TurbineVersion {
+    fn from(value: Dependency) -> Self {
+        match value {
+            Dependency::Path(path) => Self::Path(path.to_string_lossy().into_owned()),
+            Dependency::Git {
+                url,
+                rev,
+                branch,
+                tag,
+            } => Self::Git {
+                url,
+                rev,
+                branch,
+                tag,
+            },
+            Dependency::CratesIo => {
+                panic!("turbine crate not yet published to crates.io");
+            }
         }
     }
 }
 
+impl Display for TurbineVersion {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Path(path) => f.write_fmt(format_args!(r##"{{ path = "{path}" }}"##)),
+            Self::Git {
+                url,
+                rev,
+                branch,
+                tag,
+            } => {
+                let mut spec = format!(r##"{{ git = "{url}" "##);
+
+                if let Some(rev) = rev {
+                    spec.push_str(&format!(r##", rev = "{rev}" "##));
+                }
+
+                if let Some(branch) = branch {
+                    spec.push_str(&format!(r##", branch = "{branch}" "##));
+                }
+
+                if let Some(tag) = tag {
+                    spec.push_str(&format!(r##", tag = "{tag}" "##));
+                }
+
+                spec.push_str(" }");
+
+                f.write_str(&spec)
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Versions {
     error_stack: String,
     hashbrown: String,
@@ -79,7 +121,30 @@ struct Versions {
     turbine: TurbineVersion,
 }
 
-struct Template {
+impl Versions {
+    fn latest(config: &Config) -> Result<Self, Error> {
+        let error_stack = fetch_version("error-stack")?;
+        let hashbrown = fetch_version("hashbrown")?;
+        let serde = fetch_version("serde")?;
+        let serde_json = fetch_version("serde_json")?;
+        let turbine = TurbineVersion::from(config.turbine.clone());
+
+        Ok(Self {
+            error_stack,
+            hashbrown,
+            serde,
+            serde_json,
+            turbine,
+        })
+    }
+}
+
+#[derive(Debug, Template)]
+#[template(path = "Cargo.toml.askama", escape = "none")]
+struct CargoTemplate {
     name: String,
     versions: Versions,
 }
+
+#[cfg(test)]
+mod tests {}
