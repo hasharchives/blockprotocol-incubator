f62cac3d1cee9b83bd736bf5abe1cf2cf3e844fb
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-06-05 17:03:55 +0200

    feat: create `View`

--- libs/turbine-transformer/src/lib.rs
+++ libs/turbine-transformer/src/lib.rs
@@ -1,14 +1,69 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![no_std]
+
+extern crate alloc;
+
+use alloc::collections::{BTreeMap, BTreeSet};
+use petgraph::Graph;
+use petgraph::graph::NodeIndex;
+use petgraph::visit::NodeFiltered;
+use turbine::entity::{Entity, EntityId, EntityVertex};
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct EntityNode {
+    id: EntityId
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum LinkEdge {
+    Left,
+    Right
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct View {
+    graph: Graph<EntityNode, LinkEdge>,
+
+    lookup: BTreeMap<EntityId, NodeIndex>,
+    exclude: BTreeSet<NodeIndex>
+}
+
+impl View {
+    fn empty() -> Self {
+        Self {
+            graph: Graph::new(),
+            lookup: BTreeMap::new(),
+            exclude: BTreeSet::new()
+        }
+    }
+
+    fn get_or_create(&mut self, entity: EntityId) -> NodeIndex {
+        if let Some(node) = self.lookup.get(&entity) {
+            return *node;
+        }
+
+        let node = self.graph.add_node(EntityNode {
+            id: entity
+        });
+
+        self.lookup.insert(entity, node);
+
+        node
+    }
+
+    fn new(entities: &[Entity]) -> Self {
+        let mut this = Self::empty();
+
+        for entity in entities {
+            let node = this.get_or_create(entity.metadata.record_id.entity_id);
+
+            if let Some(link_data) = entity.link_data {
+                let lhs = this.get_or_create(link_data.left_entity_id);
+                let rhs = this.get_or_create(link_data.right_entity_id);
+
+                this.graph.add_edge(lhs, node, LinkEdge::Left);
+                this.graph.add_edge(node, rhs, LinkEdge::Right);
+            }
+        }
+
+        this
     }
 }
